75a1dc266118bd99b79db5a88e4682ed3f4756f8
qwackididuck <maret.benoit.pro@gmail.com>
2025-08-29 16:49:08 +0200

    feat: update url for prod

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import * as Sentry from "@sentry/react";
 Sentry.init({
     dsn: "https://07ad85d879981f57794d6f4f47fea24e@o4509927771930626.ingest.de.sentry.io/4509927775404112",
     integrations: [Sentry.browserTracingIntegration()],
-    tracesSampleRate: 1.0  // = 100%
+    tracesSampleRate: 0.1  // = 10%
 });
 
 createRoot(document.getElementById('root')!).render(
